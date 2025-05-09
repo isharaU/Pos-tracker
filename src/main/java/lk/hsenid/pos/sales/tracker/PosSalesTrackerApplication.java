598bd43da729e81c844566e4437058438b9a92e9
YisharaU <wellahewawiud.20@uom.lk>
2025-05-09 10:32:16 +0530

    initial commit

---
+++ src/main/java/lk/hsenid/pos/sales/tracker/PosSalesTrackerApplication.java
@@ -0,0 +1,13 @@
+package lk.hsenid.pos.sales.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSalesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosSalesTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/hsenid/pos/sales/tracker/PosSalesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.hsenid.pos.sales.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSalesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
