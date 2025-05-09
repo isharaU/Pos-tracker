88779d75ee7fc055b0503d995575e70646c00c62
YisharaU <wellahewawiud.20@uom.lk>
2025-05-09 11:04:06 +0530

    add DaoAuthenticationProvider

---
+++ src/main/java/lk/hsenid/pos/sales/tracker/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package lk.hsenid.pos.sales.tracker.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userDetailsService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/sales/**").hasAnyAuthority("CASHIER", "STORE_MANAGER")
+                        .requestMatchers("/api/products/**").hasAnyAuthority("STORE_MANAGER", "HEAD_OFFICE_MANAGER")
+                        .requestMatchers("/api/summaries/**").hasAuthority("HEAD_OFFICE_MANAGER")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()); // ✅ Spring Security 6+ style
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
