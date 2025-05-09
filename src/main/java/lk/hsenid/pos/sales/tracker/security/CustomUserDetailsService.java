7bb54a0435afcdd61c10a5224698e6f1e8e7ef2f
YisharaU <wellahewawiud.20@uom.lk>
2025-05-09 11:00:38 +0530

    add CustomUserDetailsService

---
+++ src/main/java/lk/hsenid/pos/sales/tracker/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package lk.hsenid.pos.sales.tracker.security;
+
+import lk.hsenid.pos.sales.tracker.entity.User;
+import lk.hsenid.pos.sales.tracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new UserDetailsImpl(user);
+    }
+}
