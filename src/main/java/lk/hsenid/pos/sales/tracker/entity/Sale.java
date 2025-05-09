1811fee025807ddf43e57cdd42d0918b47fa5f20
YisharaU <wellahewawiud.20@uom.lk>
2025-05-09 10:37:17 +0530

    add sale entity

---
+++ src/main/java/lk/hsenid/pos/sales/tracker/entity/Sale.java
@@ -0,0 +1,27 @@
+package lk.hsenid.pos.sales.tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Store store;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantity;
+    private LocalDateTime timestamp;
+}
